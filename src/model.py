d0543e00d829f8c3b17f1d35e5fd77391913bf77
Rithvik Bobbili <rithvikbobbili@gmail.com>
2021-10-06 14:43:47 -0400

    initial

--- src/model.py
+++ src/model.py
@@ -1,14 +1,21 @@
 # edit this file to create a simple model for your dataset.
 
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 class MyModel(tf.keras.Model):
 
   def __init__(self):
     super(MyModel, self).__init__()
-    self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
-    self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
+    self.dense1 = tf.keras.layers.Dense(5, activation=tf.nn.relu)
+    self.dense2 = tf.keras.layers.Dense(10, activation=tf.nn.relu)
+    self.dense3 = tf.keras.layers.Dense(3, activation=tf.nn.softmax)
 
   def call(self, inputs):
     x = self.dense1(inputs)
-    return self.dense2(x)
+    x = self.dense2(x)
+    return self.dense3(x)
+
+iris_model = MyModel(name='iris_model')
--- test/test_model.py
+++ test/test_model.py
@@ -1,6 +1,9 @@
 import pytest
 from model import MyModel
 from tensorflow.keras.models import Model
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 @pytest.fixture
@@ -8,10 +11,47 @@ def my_model():
     return MyModel()
 
 @pytest.fixture
-# def my_dataset():
+def my_dataset():
     # return your dataset object
+    iris = load_iris()
+
+    # Load data into a DataFrame
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    # Convert datatype to float
+    df = df.astype(float)
+    # append "target" and name it "label"
+    df['label'] = iris.target
+    # Use string label instead
+    df['label'] = df.label.replace(dict(enumerate(iris.target_names)))
+
+    # label -> one-hot encoding
+    label = pd.get_dummies(df['label'])
+    label.columns = ['label_' + str(x) for x in label.columns]
+    df = pd.concat([df, label], axis=1)
+    # drop old label
+    df.drop(['label'], axis=1, inplace=True)
+
+    # Creating X and y
+    X = df[['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']]
+    # Convert DataFrame into np array
+    X = np.asarray(X)
+    y = df[['label_setosa', 'label_versicolor', 'label_virginica']]
+    # Convert DataFrame into np array
+    y = np.asarray(y)
+
+    # Creating X and y
+    X = df[['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']]
+    # Convert DataFrame into np array
+    X = np.asarray(X)
+    y = df[['label_setosa', 'label_versicolor', 'label_virginica']]
+    # Convert DataFrame into np array
+    y = np.asarray(y)
+    
+    return X, y
 
 def test_model_is_model_instance(my_model):
     assert isinstance(my_model, Model)
 
-# def test_model_with_dataset_for_xxxx(my_model, my_dataset):
+def test_model_with_dataset_for_xxxx(my_model, my_dataset):
+    prediction = my_model.predict(x=my_dataset)
+    assert(prediction == 'setosa' or prediction == 'versicolor' or prediction == 'virginica')
