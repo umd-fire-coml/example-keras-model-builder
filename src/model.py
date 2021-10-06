936c331cd155b74cc24727f230f4849014ab6727
Timothy Clifford <tcliff30@terpmail.umd.edu>
2021-10-06 14:33:16 -0400

    1

--- src/model.py
+++ src/model.py
@@ -1,6 +1,59 @@
 # edit this file to create a simple model for your dataset.
 
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+iris = load_iris()
+
+# Load data into a DataFrame
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+# Convert datatype to float
+df = df.astype(float)
+# append "target" and name it "label"
+df['label'] = iris.target
+# Use string label instead
+df['label'] = df.label.replace(dict(enumerate(iris.target_names)))
+
+# label -> one-hot encoding
+label = pd.get_dummies(df['label'])
+label.columns = ['label_' + str(x) for x in label.columns]
+df = pd.concat([df, label], axis=1)
+# drop old label
+df.drop(['label'], axis=1, inplace=True)
+
+# Creating X and y
+X = df[['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']]
+# Convert DataFrame into np array
+X = np.asarray(X)
+y = df[['label_setosa', 'label_versicolor', 'label_virginica']]
+# Convert DataFrame into np array
+y = np.asarray(y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X,
+  Y,
+  test_size=0.25
+)
+
+# Passing a list of layers to the constructor
+model = Sequential([
+    Dense(5, activation='relu', input_shape=(4,)),
+    Dense(10, activation='relu'),
+    Dense(3, activation='softmax'),
+])
+model.summary()
+
+# Adding layer via add() method
+model = Sequential()
+model.add(Dense(5, activation='relu', input_shape=(4,)))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+model.summary()
 
 class MyModel(tf.keras.Model):
 
@@ -12,3 +65,4 @@ class MyModel(tf.keras.Model):
   def call(self, inputs):
     x = self.dense1(inputs)
     return self.dense2(x)
+    
