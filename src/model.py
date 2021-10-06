b10cf289e6d44c45124e0b0af79a035d4fd85688
npangaro <90272870+npangaro@users.noreply.github.com>
2021-10-06 14:42:28 -0400

    Nick

--- src/model.py
+++ src/model.py
@@ -1,6 +1,15 @@
 # edit this file to create a simple model for your dataset.
 
 import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# Construct a tf.data.Dataset
+ds = tfds.load('mnist', split='train', shuffle_files=True)
+
+# Build your input pipeline
+ds = ds.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
+for example in ds.take(1):
+  image, label = example["image"], example["label"]
 
 class MyModel(tf.keras.Model):
 
--- test/test_model.py
+++ test/test_model.py
@@ -8,10 +8,12 @@ def my_model():
     return MyModel()
 
 @pytest.fixture
-# def my_dataset():
-    # return your dataset object
+def my_dataset():
+    my_dataset = model.ds
+    return my_dataset
 
 def test_model_is_model_instance(my_model):
     assert isinstance(my_model, Model)
 
-# def test_model_with_dataset_for_xxxx(my_model, my_dataset):
+def test_model_with_dataset_for_xxxx(my_model, my_dataset):
+    assert my_model.get_layer(name=None, index=0)
