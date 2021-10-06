c7737f99b5e261a4429bdf5e6533057130768639
Matthew Snyder <msnyder2@terpmail.umd.edu>
2021-10-06 14:34:26 -0400

    changes

--- src/model.py
+++ src/model.py
@@ -6,9 +6,16 @@ class MyModel(tf.keras.Model):
 
   def __init__(self):
     super(MyModel, self).__init__()
-    self.dense1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
-    self.dense2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
+    self.r1 = tf.keras.layers.Dense(4, activation=tf.nn.relu)
+    self.r2 = tf.keras.layers.Dense(10, activation=tf.nn.relu)
+    self.sm1 = tf.keras.layers.Dense(3, activation=tf.nn.softmax)
+    self.sm2 = tf.keras.layers.Dense(5, activation=tf.nn.softmax)
 
   def call(self, inputs):
-    x = self.dense1(inputs)
-    return self.dense2(x)
+    x = inputs
+    for _ in range(5):
+      x = self.r1(inputs)
+      x = self.sm1(inputs)
+      x = self.r2(inputs)
+      x = self.sm2(inputs)
+    return x
--- test/test_model.py
+++ test/test_model.py
@@ -11,7 +11,10 @@ def my_model():
 # def my_dataset():
     # return your dataset object
 
-def test_model_is_model_instance(my_model):
+def test_model_is_model_instance(my_model: MyModel):
     assert isinstance(my_model, Model)
+
+def test_model_returns_same_type(my_model: MyModel):
+    assert my_model.call([1,2,3,4]) is not None
 
 # def test_model_with_dataset_for_xxxx(my_model, my_dataset):
