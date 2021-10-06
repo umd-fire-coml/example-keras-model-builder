aa6febf3d983a89895fd482095ef5ab8e0d5caa1
Raymond H Tu <hh2@umd.edu>
2021-10-06 13:33:01 -0400

    init

---
+++ test/test_model.py
@@ -0,0 +1,17 @@
+import pytest
+from model import MyModel
+from tensorflow.keras.models import Model
+
+
+@pytest.fixture
+def my_model():
+    return MyModel()
+
+@pytest.fixture
+# def my_dataset():
+    # return your dataset object
+
+def test_model_is_model_instance(my_model):
+    assert isinstance(my_model, Model)
+
+# def test_model_with_dataset_for_xxxx(my_model, my_dataset):
